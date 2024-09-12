1862fd535860de257b09263d4555f42092d3bf41
Paulo Orquillo <paulo.orquillo@gmail.com>
2024-09-12 23:54:20 +1200

    initial commit

---
+++ app/callback/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+//http://localhost:3000/callback?code=aPrxzYrgkWdzMCdPUNhgNY6Nd1NtCVYsaKeufqbdXBxpM47Ad.AVzAyahOJQ3rIX1IlSPoJHVw%3D%3D
+export default function Page() {
+  return <div>callback</div>;
+}
